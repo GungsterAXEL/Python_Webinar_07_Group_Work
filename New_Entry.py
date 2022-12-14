bbcbe4ea3d533a82a5f7edf28a8fd9fd383f6443
Gun`gster <gungster.axel.ru@mail.ru>
2022-12-15 00:23:37 +0300

    Logger and logging added

--- New_Entry.py
+++ New_Entry.py
@@ -3,10 +3,17 @@
 На входе минимум 4 переменных: Имя, Фамилия, номер и примечание.
 '''
 import os
+import Logger
 
 os.chdir(os.path.dirname(__file__))
 
 def new_entry_saver(first_name, last_name, tel_number, footnote):
-	new_entry = f'\n{first_name}, {last_name}, {tel_number}, {footnote};\n'
-	with open('PhoneBook.txt', 'a', encoding='utf-8') as phone_data:
-		phone_data.write(new_entry)
+	new_entry = f'{first_name}, {last_name}, {tel_number}, {footnote};\n'
+	try:
+		#with open('PhoneBook.txt', 'a', encoding='utf-8') as phone_data:
+			phone_data.write(new_entry)
+			Logger.log_logger('New_Entry_Saver', True)
+	except:
+		Logger.log_logger('New_Entry_Saver', False)
+
+new_entry_saver('Name', 'LastName', '+7', 'Sexy')
