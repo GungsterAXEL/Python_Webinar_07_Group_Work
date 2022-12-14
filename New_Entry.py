70c8a5c6689e84b75d28e09663fe5c01e6266287
Gun`gster <gungster.axel.ru@mail.ru>
2022-12-15 01:01:01 +0300

    Some fixes

--- New_Entry.py
+++ New_Entry.py
@@ -10,10 +10,8 @@ os.chdir(os.path.dirname(__file__))
 def new_entry_saver(first_name, last_name, tel_number, footnote):
 	new_entry = f'{first_name}, {last_name}, {tel_number}, {footnote};\n'
 	try:
-		#with open('PhoneBook.txt', 'a', encoding='utf-8') as phone_data:
+		with open('PhoneBook.txt', 'a', encoding='utf-8') as phone_data:
 			phone_data.write(new_entry)
 			Logger.log_logger('New_Entry_Saver', True)
 	except:
 		Logger.log_logger('New_Entry_Saver', False)
-
-new_entry_saver('Name', 'LastName', '+7', 'Sexy')
