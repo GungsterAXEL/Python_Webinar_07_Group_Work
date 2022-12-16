580f529efdfe027660a1eabcf27270a73f06c065
Yaroslav.S <sysoev.yar@yandex.ru>
2022-12-17 01:19:43 +0500

    готово модуль поиска и принт_формат

---
+++ Finder.py
@@ -0,0 +1,23 @@
+import os
+import Logger
+
+os.chdir(os.path.dirname(__file__))
+
+
+def find_finder():
+    try:
+        word = input("Введи искомое значение -> ")
+        with open('PhoneBook.txt', 'r', encoding='utf-8') as file:
+            content = list(map(str, file.read().split('\n')))
+        temp = []
+        for i, v in enumerate(content):
+            if word.lower() in v.lower():
+                temp.append(v)
+        if len(temp) == 0:
+            print("Ничего не найдено!")
+        else:
+            for i in temp:
+                print(i)
+        Logger.log_logger('Find_Finder', True)
+    except:
+        Logger.log_logger('Find_Finder', False)
--- Print_Format.py
+++ Print_Format.py
@@ -3,14 +3,12 @@ import os
 
 os.chdir(os.path.dirname(__file__))
 
-def import_from_file():
+
+def print_format_columns():
     with open("PhoneBook.txt", "r", encoding='utf-8') as phonebook:
         phone = phonebook.readlines()
-        for i in range(len(phone)):
+        for i in range(1, len(phone)):
             wrds = phone[i].replace(';', '').split(', ')
             for j in range(len(wrds)):
                 wrds[j].replace('; ', ' ')
                 print(wrds[j])
-
-
-import_from_file()
